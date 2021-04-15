2743ced63fe9891f57abb018abdc8f14eb24634a
ShiKirill <shiraevkirill@gmail.com>
2021-04-16 02:38:27 +0700

    Task 2

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+
+class Program
+{
+  static void Main()
+  {
+
+
+    string jsonString = File.ReadAllText("test.json");
+    var o = JObject.Parse(jsonString);
+
+
+    foreach (var x in o)
+    {
+      string name = x.Key;
+      JToken value = x.Value;
+      foreach (var i in value)
+      {
+        Console.WriteLine("{0,-20} || {1,15}", name, i);
+      }
+    }
+
+  }
+
+
+}
